e663a6cf477a4142f741a4aeec3e9714f1756172
Ranjitha1412 <51219884+Ranjitha1412@users.noreply.github.com>
2019-06-19 22:20:49 +0530

    Update s5.py

--- s5.py
+++ s5.py
@@ -1,11 +1,12 @@
 num=input().split(" ")
 num=map(int,num)
-if(num[0]>num[1]):
-  if(num[0]>num[2]):
-    print(num[0])
+if(num[0]>0 and num[1]> and num[2]>0):
+  if(num[0]>num[1]):
+    if(num[0]>num[2]):
+      print(num[0])
+    else:
+      print(num[2])
+  elif(num[1]>num[2]):
+    print(num[1])
   else:
     print(num[2])
-elif(num[1]>num[2]):
-  print(num[1])
-else:
-  print(num[2])
